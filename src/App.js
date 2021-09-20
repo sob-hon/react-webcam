27f7384aa9429d1e7cfbe4fe5422a24b3e5b2acc
Sobhan <sobhanyazdanjo@gmail.com>
2021-09-20 20:06:02 +0430

    Add webcam component and button and functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home/Home'
-import Footer from './components/Footer'
+import { WebcamCapture } from './components/Webcam/Webcam';
+
+
 function App() {
   return (
     <div className="App">
-      <Home/>
-      <Footer/>
+      <WebcamCapture />
     </div>
   );
 }
